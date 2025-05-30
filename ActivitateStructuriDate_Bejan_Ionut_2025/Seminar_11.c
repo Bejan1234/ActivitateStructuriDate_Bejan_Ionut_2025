3e7a406e0777f04cdc34196760e49457c763d3bc
BejanIonut <ionutbejan678@gmail.com>
2025-05-30 17:09:39 +0300

    Seminar 11 - Stack & Queue

--- ActivitateStructuriDate_Bejan_Ionut_2025/Seminar_11.c
+++ ActivitateStructuriDate_Bejan_Ionut_2025/Seminar_11.c
@@ -6,7 +6,7 @@
 //trebuie sa folositi fisierul masini.txt
 //sau va creati un alt fisier cu alte date
 
-struct StructuraMasina {
+struct StructuraMasina { //Seminar 11 - Stack & Queue
 	int id;
 	int nrUsi;
 	float pret;
